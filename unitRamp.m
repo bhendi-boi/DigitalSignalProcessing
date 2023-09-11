739334382afb6941768a78d6a0038b2f987e0f47
bhendi-boi <bhendiisgreen@gmail.com>
2023-09-11 13:51:27 +0530

    Made dt optional in unitStep and unitRamp

--- unitRamp.m
+++ unitRamp.m
@@ -1,4 +1,7 @@
 function [x,n] = unitRamp(nmin,nmax,dt)
+    if nargin == 2
+        dt = 1;
+    end
    n = nmin:dt:nmax;
    [unitStepTemp,t ]= unitStep(nmin,nmax,dt);
    x = n .* unitStepTemp;
--- unitStep.m
+++ unitStep.m
@@ -1,4 +1,7 @@
 function [x,n] = unitStep(nmin,nmax,dt)
+   if nargin == 2
+       dt = 1;
+   end
    n = nmin:dt:nmax;
    x = 1 * (n >= 0);
 end
