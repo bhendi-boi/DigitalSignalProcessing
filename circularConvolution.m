c7f164a3ba55f6de6c9e7de1a28c8d61df28547c
bhendi-boi <bhendiisgreen@gmail.com>
2023-09-25 13:23:14 +0530

    formatted conv and cconv

--- circularConvolution.m
+++ circularConvolution.m
@@ -1,18 +1,24 @@
-function [y] = circularConvolution(x,h)
-    [rows1, columns1] = size(x);
-    [rows2, columns2] = size(h);
-    
+function [y] = circularConvolution(x, h)
+    [~, columns1] = size(x);
+    [~, columns2] = size(h);
+
     % last index of y(n)
     lastIndex = columns1 + columns2 - 1;
-    y = zeros(1,columns2);
+    y = zeros(1, columns2);
+
     for n = 1:lastIndex
         temp = 0;
+
         for k = 1:columns1
-            if n - k  >= 0 && n - k + 1<= columns2
-                temp = temp + x(k) * h(n-k+1);
+
+            if n - k >= 0 && n - k + 1 <= columns2
+                temp = temp + x(k) * h(n - k + 1);
             end
+
         end
-        idx = 1 + rem(n - 1,columns2);
+
+        idx = 1 + rem(n - 1, columns2);
         y(idx) = y(idx) + temp;
     end
+
 end
