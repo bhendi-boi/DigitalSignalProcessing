98ce1b2b346191a71bf267b04963d4cde673bcd7
bhendi-boi <bhendiisgreen@gmail.com>
2023-09-25 12:52:13 +0530

    added the ability to shift impulse

--- impulse.m
+++ impulse.m
@@ -1,4 +1,9 @@
-function [x,n] = impulse(nmin,nmax,dt)
-   n = nmin:dt:nmax;
-   x = (n == 0);
+function [x, n] = impulse(nmin, nmax, dt, n0)
+
+    if nargin == 3
+        n0 = 0;
+    end
+
+    n = nmin:dt:nmax;
+    x = (n == n0);
 end
