f2447800c85aaa2c8984561ec34b786d7e0beecc
bhendi-boi <bhendiisgreen@gmail.com>
2023-10-16 15:24:09 +0530

    Added msePsnr.m

---
+++ msePsnr.m
@@ -0,0 +1,7 @@
+function [mse,psnr] = msePsnr(ref,noise)
+%msePsnr Finds mean square error and peak signal to noise ratio given 2
+%grayscale images
+%  
+mse = (norm(ref(:)-noise(:),2).^2)/numel(ref);
+psnr = 10 * log(65025/mse);
+end
